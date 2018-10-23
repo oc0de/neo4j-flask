c10b83408876e3ce3f140a26aa50277c076ef257
3hsan <3hsan.v@gmail.com>
2018-10-22 21:39:07 -0500

    update

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+import sys
+
 from blog import app
 import os
 
 app.secret_key = os.urandom(24)
-app.run()
+# port = int(os.environ.get("PORT", 5000))
+port = int(sys.argv[1].split("=")[1])
+app.run(host='0.0.0.0', port=port)
