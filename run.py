fdca39f1ec874a9e02765b8bbcff14f363ea9ab0
3hsan <3hsan.v@gmail.com>
2018-10-22 21:33:58 -0500

    Add runtime

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-import sys
-
 from blog import app
 import os
 
 app.secret_key = os.urandom(24)
-# port = int(os.environ.get("PORT", 5000))
-port = int(sys.argv[1].split("=")[1])
-app.run(host='0.0.0.0', port=port)
+app.run()
